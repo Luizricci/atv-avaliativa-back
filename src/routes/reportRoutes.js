9c555e3e8ee4c76ccf9ffaa2ba8d3db04ccd64b6
Luiz_ricci <luizaureliano.tds1.senai@gmail.com>
2025-04-29 11:06:12 -0300

    Feat: add api key in reportRoutes

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require("../controllers/reportControllers");
+const apikey = require("../config/apiKey");
 
+
+router.use(apikey);
 router.get("/pdf", reportController.exportHeroPDF);
 
 module.exports = router;
